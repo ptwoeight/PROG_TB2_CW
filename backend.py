25f7bf97927f7548fa41fcef39c05b318fe19bdc
ptwoeight <109756794+ptwoeight@users.noreply.github.com>
2024-02-15 01:12:09 +0000

    task 1 but it printed weirdly

---
+++ backend.py
@@ -0,0 +1,43 @@
+class SmartPlug:    # Task 1
+    def __init__(self, consumptionRate):
+        self.consumptionRate = consumptionRate
+        self.switchedOn = False
+
+    def toggleSwitch(self):
+        self.switchedOn = not self.switchedOn
+    
+    def getSwitchedOn(self):
+        return self.switchedOn
+
+    def getConsumptionRate(self):
+        if self.consumptionRate <= 150 and self.consumptionRate >= 0:
+            return self.getConsumptionRate
+        else:
+            errorMessage = "Invalid consumption rate."    # Check if this is okay
+            return errorMessage
+
+    def setConsumptionRate(self, rate):
+        if rate <= 150 and rate >= 0:
+            self.consumptionRate = rate
+        else:
+            errorMessage = "Rate is out of bounds."
+            return errorMessage
+
+    def __str__(self):
+        output = f"Current consumption rate: {self.consumptionRate} | Switch 'ON' status: {self.switchedOn}"
+        return output
+
+# Testing SmartPlug
+def testSmartPlug():
+    mySmartPlug = SmartPlug(45)
+    mySmartPlug.toggleSwitch()
+    print(mySmartPlug.getSwitchedOn())
+    print(mySmartPlug.getConsumptionRate())  
+    mySmartPlug.setConsumptionRate(77)
+    print(mySmartPlug.getConsumptionRate())
+    print(mySmartPlug)
+
+
+
+# TEST RUN
+testSmartPlug()
